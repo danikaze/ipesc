f35cad21c7dda348b31709ea539dd61698b0f5e3
danikaze <danikaze@gmail.com>
2023-10-18 00:57:26 +0900

    Handle css modules

--- webpack/base.ts
+++ webpack/base.ts
@@ -4,6 +4,7 @@ import * as TerserPlugin from 'terser-webpack-plugin';
 import * as MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 import { getFileComment } from './utils/file-comment';
+import { getCssLocalIdent } from './utils/get-css-local-ident';
 import { getDateString } from './utils/get-date-string';
 import { jsonify } from './utils/jsonify';
 
@@ -57,13 +58,35 @@ export const webpackConfig: (fn: WebpackConfigCreator) => () => Configuration = 
     ],
     module: {
       rules: [
+        // code
         {
           test: /\.(ts|tsx)$/i,
           loader: 'ts-loader',
           exclude: ['/node_modules/'],
         },
+        // css modules
         {
-          test: /\.(css|scss|less)$/i,
+          test: /\.s?css$/,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                modules: {
+                  getLocalIdent: getCssLocalIdent(),
+                },
+                sourceMap: true,
+                importLoaders: 1,
+              },
+            },
+            'sass-loader',
+          ],
+          include: /\.module\.s?(c|a)ss$/,
+        },
+        // css (non-module)
+        {
+          test: /\.(css|scss|less)$/,
+          exclude: /\.module\.(css|scss|less)$/,
           use: [
             MiniCssExtractPlugin.loader,
             'css-loader',
@@ -71,6 +94,7 @@ export const webpackConfig: (fn: WebpackConfigCreator) => () => Configuration = 
             'sass-loader',
           ],
         },
+        // assets
         {
           test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
           type: 'asset',
