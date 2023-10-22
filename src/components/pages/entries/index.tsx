9fc15787247ef0847b83f4b448db5dacfc4c2fb8
danikaze <danikaze@gmail.com>
2023-10-23 00:44:56 +0900

    Refactor WaitForData component wrapper

--- src/components/pages/entries/index.tsx
+++ src/components/pages/entries/index.tsx
@@ -1,11 +1,11 @@
-import { FC, ReactNode } from 'react';
-import { Flex, Heading, Spinner } from '@chakra-ui/react';
+import { FC } from 'react';
+import { Heading } from '@chakra-ui/react';
 
-import { ProcessedData } from 'data/types';
 import { Page } from 'components/page';
 import { ParticipationPerSeasonChart } from 'components/charts/participation-per-season';
 import { ParticipationPerRaceChart } from 'components/charts/participation-per-race';
 import { DataFilter } from 'components/data-filter';
+import { WaitForData } from 'components/wait-for-data';
 
 import { useEntriesPage } from './hooks';
 
@@ -28,18 +28,5 @@ export const EntriesPage: FC = () => {
         <ParticipationPerRaceChart data={raceData!} />
       </WaitForData>
     </Page>
-  );
-};
-
-const WaitForData: FC<{ data: ProcessedData | undefined; children: ReactNode }> = ({
-  data,
-  children,
-}) => {
-  if (data) return children;
-
-  return (
-    <Flex width='100%' justifyContent='center' alignItems='center' my={4}>
-      <Spinner size='xl' thickness='5px' color='orange' label='Loading...' />
-    </Flex>
   );
 };
