1626ea1e32dbda38ba9eb226a24e57f0ca565131
danikaze <danikaze@gmail.com>
2023-10-31 20:55:20 +0900

    Add S7.5 MX-5 season (race 1)

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -16,6 +16,7 @@ export const CUSTOM_NAMES: Record<string, string> = {
   HmJXSR2OksoGBlT10eR_q: 'BTCC',
   hxjmRD7aBYGUsyiNlCVo3: 'S6',
   u1Alc5D09NQjUNC4xPSVm: 'S7',
+  V2Dk0HTCw4sMCFvTyW0r8: 'Post S7 MX5 micro-season',
 };
 
 export const TRACK_MAX_BEST_TIMES = 100;
