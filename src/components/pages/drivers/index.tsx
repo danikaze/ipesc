9da097bb53a5a2ad7352b21f6e9b256d7604787e
danikaze <danikaze@gmail.com>
2023-11-10 17:47:12 +0900

    Reset the filter data if game changes (and other small improvements on DriverRank chart)

--- src/components/pages/drivers/index.tsx
+++ src/components/pages/drivers/index.tsx
@@ -1,7 +1,6 @@
 import { FC } from 'react';
 import { Heading } from '@chakra-ui/react';
 
-import { Game } from 'data/types';
 import { Page } from 'components/page';
 import { WaitForData } from 'components/wait-for-data';
 import { DataFilter } from 'components/data-filter';
@@ -10,8 +9,15 @@ import { DriversRankChart } from 'components/charts/drivers-rank-chart';
 import { useDriversPage } from './hooks';
 
 export const DriversPage: FC = () => {
-  const { filteredData, isAccSelected, filterChangeTime, setFilter } = useDriversPage();
-  console.log(filteredData);
+  const {
+    filter,
+    updateFilter,
+    filteredData,
+    isAccSelected,
+    filterChangeTime,
+    namedSeasons,
+  } = useDriversPage();
+
   return (
     <Page>
       <Heading size='md' marginBottom={4}>
@@ -22,8 +28,9 @@ export const DriversPage: FC = () => {
           showChampionships
           showGame
           showAccVersion={isAccSelected}
-          onChange={setFilter}
-          defaultValue={{ game: Game.ACC }}
+          championshipList={namedSeasons}
+          onChange={updateFilter}
+          value={filter}
         />
         <DriversRankChart key={filterChangeTime} data={filteredData!} />
       </WaitForData>
