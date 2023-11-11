3dbb1fb27c612a9fdf03d15a0d1a0376cba3e3c2
danikaze <danikaze@gmail.com>
2023-11-12 00:32:22 +0900

    Group sgp data conversion functions

--- src/data/process.ts
+++ src/data/process.ts
@@ -3,19 +3,18 @@ import { dirname, join, relative } from 'path';
 import { sync as mkdirpSync } from 'mkdirp';
 
 import { SgpChampionshipApiData } from 'utils/sgp/championship-api-data';
-import { SgpCategory, SgpEventType, SgpGame } from 'utils/sgp/types';
+import { SgpEventType } from 'utils/sgp/types';
 import { SgpEventApiData } from 'utils/sgp/event-api-data';
 import { dropUndefinedFields } from 'utils/drop-undefined-fields';
 import { Timestamp } from 'utils/types';
 
+import { sgpGame2Game, sgpCategory2Category, sgpEventType2EventType } from './sgp';
 import { CUSTOM_NAMES, OUTPUT_FILE, RAW_DATA_DIR, ROOT_PATH } from './constants';
 import {
   Car,
-  Category,
   Championship,
   Driver,
   Event,
-  Game,
   ProcessedData,
   TrackRecord,
   TrackData,
@@ -279,22 +278,4 @@ function eventResult2trackRecord(
   });
 
   return record;
-}
-
-function sgpGame2Game(game: SgpGame | undefined): Game | undefined {
-  if (game === SgpGame.ACC) return Game.ACC;
-  if (game === SgpGame.AC) return Game.AC;
-  if (game === SgpGame.RACE_ROOM) return Game.RACE_ROOM;
-  if (game === SgpGame.AUTOMOBILISTA_2) return Game.AUTOMOBILISTA_2;
-  if (game === SgpGame.F1_23) return Game.F1_23;
-}
-
-function sgpCategory2Category(cat: SgpCategory | undefined): Category | undefined {
-  if (!cat) return;
-  return cat[0].toUpperCase() as Category;
-}
-
-function sgpEventType2EventType(type: SgpEventType): EventType | undefined {
-  if (type === SgpEventType.QUALI) return 'quali';
-  if (type === SgpEventType.RACE) return 'race';
 }
